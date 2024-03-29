f6a365e5753181b82d85bd4afd589e91eba96956
Shormily <shormilyrais@gmail.com>
2024-03-29 20:13:30 +0600

    check

--- app/Header/Tables/TableFour.tsx
+++ app/Header/Tables/TableFour.tsx
@@ -1,6 +1,7 @@
 import { BRAND } from "@/types/brand";
 import Image from "next/image";
 import DropdownDefault from "../Dropdowns/DropdownDefault";
+// import DropdownDefault from "../Dropdowns/DropdownDefault";
 
 const brandData: BRAND[] = [
   {
