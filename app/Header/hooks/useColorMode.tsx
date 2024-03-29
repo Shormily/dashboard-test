d32f640140fb01e9412ef08d8e70d0c2c815adf9
Shormily <shormilyrais@gmail.com>
2024-03-29 23:25:36 +0600

    first commit

--- app/Header/hooks/useColorMode.tsx
+++ app/Header/hooks/useColorMode.tsx
@@ -1,16 +1,19 @@
-import { useEffect } from "react";
-import useLocalStorage from "./useLocalStorage";
+import { useEffect } from 'react';
+import useLocalStorage from './useLocalStorage';
 
 const useColorMode = () => {
-  const [colorMode, setColorMode] = useLocalStorage("color-theme", "light");
+  const [colorMode, setColorMode] = useLocalStorage('color-theme', 'light');
 
   useEffect(() => {
-    const className = "dark";
-    const bodyClass = window.document.body.classList;
-    // const bodyClass = typeof window !== "undefined" && window.document.body.classList;
-    colorMode === "dark"
-      ? bodyClass.add(className)
-      : bodyClass.remove(className);
+    const className = 'dark';
+    if (typeof window !== 'undefined') {
+      // Code that uses window here
+      const bodyClass = window.document.body.classList;
+      // const bodyClass = typeof window !== "undefined" && window.document.body.classList;
+      colorMode === 'dark'
+        ? bodyClass.add(className)
+        : bodyClass.remove(className);
+    }
   }, [colorMode]);
 
   return [colorMode, setColorMode];
